95322582b8b3ea5dbe3d73e09dc052b728d3e18b
Alexander Zupke <94173392+Yata777@users.noreply.github.com>
2023-08-08 19:39:34 +0200

    Tests of the uescases and setup for testing finished

--- DartFlutter_Course/CleanArchitecture/advicer/lib/domain/failures/failures.dart
+++ DartFlutter_Course/CleanArchitecture/advicer/lib/domain/failures/failures.dart
@@ -1,7 +1,18 @@
-abstract class Failure{
+import 'package:equatable/equatable.dart';
 
+abstract class Failure{}
+
+class ServerFailure extends Failure with EquatableMixin {
+  @override
+  List<Object?> get props => [];
 }
 
-class ServerFailure extends Failure {}
+class GeneralFailure extends Failure with EquatableMixin{
+   @override
+  List<Object?> get props => [];
+}
 
-class GeneralFailure extends Failure{}
+class CacheFailure extends Failure with EquatableMixin{
+   @override
+  List<Object?> get props => [];
+}
